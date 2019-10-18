5ae37a4f37e87a940302684cacce5f877812a950
Onea Radu <radu-andrei.onea@my.fmi.unibuc.ro>
2019-10-18 15:06:04 +0300

    First Stage- Auth

---
+++ ListIt/Controllers/ListController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListIt.Controllers
+{
+    [Authorize]
+    public class ListController : Controller
+    {
+        // GET: List
+        public ActionResult See()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
